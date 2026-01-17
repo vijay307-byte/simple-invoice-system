f731b64bbb52567d3315cb73ad2e053125cd2f96
Vijay <vijayberva030705@gmail.com>
2026-01-17 14:45:58 +0530

    Refactor invoice generation logic in generate.php

--- generate.php
+++ generate.php
@@ -14,40 +14,47 @@
 <body class="bg-dark">
 
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') 
-{
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
     $itemCount = isset($_POST['item_count']) ? (int)$_POST['item_count'] : 0;
-    $items  = $_POST['items'] ?? [];
-    $qty   = $_POST['qty'] ?? [];
-    $price = $_POST['price'] ?? [];
 
-    $grandTotal = 0;
+            $items  = $_POST['items'] ?? [];
+            $qty   = $_POST['qty'] ?? [];
+            $price = $_POST['price'] ?? [];
 
-    echo "<div class='m-auto mt-5 p-5 card bg-dark text-light fs-4 border border-5 rounded-5 border-primary w-50'>";
-       echo "<h1 class='text-center mb-4'>Invoice</h1>";
-          for ($i = 0; $i < count($items); $i++)
+            $grandTotal = 0;
+
+            echo "<div id='invoice' class='m-auto mt-5 p-5 card bg-dark text-light fs-4 border border-5 rounded-5 border-primary w-50'>";
+            echo "<h1 class='text-center mb-4'>Invoice</h1>";
+
+            for ($i = 0; $i < count($items); $i++)
             {
-               if (
-                     empty($items[$i]) ||
-                     !isset($qty[$i], $price[$i]) ||
-                      $qty[$i] <= 0 ||
-                      $price[$i] < 0
-                    ) 
-                    {
-                            continue;
-                    }
-
                 $subtotal = $qty[$i] * $price[$i];
                 $grandTotal += $subtotal;
                 echo "<div class='mb-3 fs-1 border-bottom border-secondary'>"; 
                 echo "Item: <span class = 'fs-1'>" . $items[$i] . "</span><br>";
-                echo "Quantity: " . $qty[$i] . "<br>"; 
-                echo "Price: ₹" . $price[$i] . "<br>"; 
-                echo "Subtotal: ₹" . $subtotal . "<br>";
+                echo "Quantity: ".$qty[$i] . "<br>"; 
+                echo "Price: ₹".$price[$i] . "<br>"; 
+                echo "Subtotal: ₹".$subtotal. "<br>";
                 echo "</div>"; }
             }
             echo "<h3 class='text-end mt-4'>Total Payable: ₹{$grandTotal}</h3>";
+            echo "<button class='btn btn-primary mt-4' onclick='shot()'>Download Invoice</button>";
             echo "</div>"; 
 ?>
 </body>
 </html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
+<script>
+function shot(){
+  html2canvas(document.getElementById('invoice')).then(c=>{
+    const a = document.createElement('a');
+    a.href = c.toDataURL('image/png');
+    a.download = 'invoice.png';
+    a.click();
+  }); 
+}
+</script>
